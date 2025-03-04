2429208f192a34e1776496ca383188695827a4f5
minhNguyen85288842 <minhng@class-docker>
2025-03-04 18:02:26 +0000

    some stuff

--- src/SimplifiedThreePL.py
+++ src/SimplifiedThreePL.py
@@ -1,1 +1,12 @@
-import
+import numpy as np
+import src.Experiment as experiment
+
+class SimplifiedThreePL:
+    def __init__(self, experiment):
+        if isinstance(experiment, Experiment):
+            raise TypeError("experiment must be an instance of Experiment")
+        if not experiment.conditions:
+            raise ValueError("experiment must contain at least one sdt condition")
+        self.experiment=experiment
+
+    def summary(self):
